0319d021c227b38ab5927b66a36c8a96593d04b5
JJakDDo <kangdw124@gmail.com>
2022-10-12 00:09:20 +0900

    feat: implement player movement and first person view camera

---
+++ threejs/src/components/Player.js
@@ -0,0 +1,66 @@
+import { useFrame, useThree } from "@react-three/fiber";
+import { useSphere } from "@react-three/cannon";
+import { useEffect, useRef } from "react";
+import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
+
+const JUMP_FORCE = 3;
+const SPEED = 4;
+
+export const Player = () => {
+  const { moveForward, moveBackward, moveLeft, moveRight, jump } =
+    useKeyboard();
+  const { camera } = useThree();
+  const [ref, api] = useSphere(() => ({
+    mass: 1,
+    type: "Dynamic",
+    position: [0, 1, 0],
+  }));
+  const vel = useRef([0, 0, 0]);
+
+  useEffect(() => {
+    api.velocity.subscribe((v) => (vel.current = v));
+  }, [api.velocity]);
+
+  const pos = useRef([0, 0, 0]);
+
+  useEffect(() => {
+    api.position.subscribe((p) => (pos.current = p));
+  }, [api.position]);
+
+  useFrame(() => {
+    camera.position.copy(
+      new Vector3(pos.current[0], pos.current[1], pos.current[2])
+    );
+
+    const direction = new Vector3();
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+
+    //FIXME 현재 점프키를 연타하면 공중에서 한번 더 점프하는 버그가 있음
+    // 나중에 collision을 배우면 그라운드와 플레이어가 collision이 될 때 flag를 false를 주고
+    // 점프키를 누르면 flag를 true로 설정해서 플레이어가 그라운드에 있을 때만 점프가 가능하게 수정해준다.
+    if (jump && Math.abs(vel.current[1]) < 0.05) {
+      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+    }
+  });
+
+  return <mesh ref={ref}></mesh>;
+};
